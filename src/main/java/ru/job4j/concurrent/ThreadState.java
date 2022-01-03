c6cd0a9a0d099e420b203793c22eaaa8a4acca8d
BarmaleySPb <e.g.zakharov@gmail.com>
2022-01-03 16:52:36 +0300

    1.1. Состояние нити. [#229175 #246443]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,10 +10,12 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName());
             System.out.println(second.getName());
         }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
         System.out.println("Task completed");
     }
 }
